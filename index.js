104cd964ec0a92f2a616e52b51d42f7075b8509b
James Serengia <jamesserengia@gmail.com>
2023-11-22 12:45:38 +0300

    Update data loading startegy

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 
 const main = async () => {
-  //   const html = await request.get("https://serecode.com");
-  const res = await axios.get("https://serecode.com");
-  const html = res.data;
+  const html = await request.get("https://serecode.com");
+  //   const res = await axios.get("https://serecode.com");
+  //   const html = res.data;
+
+  const $ = cheerio.load(html);
+
+  $("p").each((_, el) => {
+    console.log($(el).text());
+  });
 
   fs.writeFileSync("./content.html", html);
 };
