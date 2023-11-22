17bd7f3a0e97d05c33b626522cc07fddcf7c4e24
James Serengia <jamesserengia@gmail.com>
2023-11-22 12:34:07 +0300

    Update to use axios

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const request = require("request-promise");
+const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
 const main = async () => {
-  const html = await request.get("https://serecode.com");
+  //   const html = await request.get("https://serecode.com");
+  const res = await axios.get("https://serecode.com");
+  const html = res.data;
 
   fs.writeFileSync("./content.html", html);
 };
